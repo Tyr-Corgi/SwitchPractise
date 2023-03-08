b58e6406883189d377822daba542bc4701869c05
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-08 10:59:58 -0700

    Personal Switch project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿    Console.WriteLine("Pick a number between 0 and 6");
+    int day = int.Parse(Console.ReadLine());
+    
+    switch (day)
+    {
+        case 0:
+            Console.WriteLine("Sunday");
+            break;
+        case 1:
+            Console.WriteLine("Monday");
+            break;
+        case 2:
+            Console.WriteLine("Tuesday");
+            break;
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+        case 4:
+            Console.WriteLine("Thursday");
+            break;
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+        default:
+            Console.WriteLine("You didn't pick a number between \'0 and 6\'");
+        break;
+
+    }
